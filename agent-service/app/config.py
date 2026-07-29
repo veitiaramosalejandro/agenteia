2996fc5be75b62728e4b78e96d26ee4b2d072c0f
Alejandro Veitia Ramos <alejandrovr891105@gmail.com>
2026-07-29 15:35:44 +0100

    cambiando a 30 minutos

--- agent-service/app/config.py
+++ agent-service/app/config.py
@@ -32,8 +32,7 @@ class Settings(BaseSettings):
     SQL_SERVER_USER: str = os.getenv("SQL_SERVER_USER", "sa")
     SQL_SERVER_PASSWORD: str = os.getenv("SQL_SERVER_PASSWORD", "Abcd*1234")
     SQL_SERVER_DB: str = os.getenv("SQL_SERVER_DB", "ISIFrameIsicom")
-    #DB_STUDY_INTERVAL_SECONDS: int = int(os.getenv("DB_STUDY_INTERVAL_SECONDS", "1800"))
-    DB_STUDY_INTERVAL_SECONDS: int = int(os.getenv("DB_STUDY_INTERVAL_SECONDS", "60"))
+    DB_STUDY_INTERVAL_SECONDS: int = int(os.getenv("DB_STUDY_INTERVAL_SECONDS", "1800"))    
     
     # Audio
     AUDIO_DIR: str = os.getenv("AUDIO_DIR", "./audio_samples")
